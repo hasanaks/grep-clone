36ca8350a760921af063aebca2a6cfbef7b5ffe4
hasanaks <hasanarda2015@gmail.com>
2022-09-30 20:06:13 +0300

    parse command line arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string_view>
 
-int main() {
-  std::cout << "Hello, World!\n";
-  return 0;
+static void find_pattern_in_file(std::string_view pattern, std::string_view file) {
+  // TODO: implement find_pattern_in_file
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "usage: grep-clone PATTERN FILE\n";
+    return 1;
+  }
+
+  const char* pattern = argv[1];
+  const char* file = argv[2];
+
+  find_pattern_in_file(pattern, file);
 }
